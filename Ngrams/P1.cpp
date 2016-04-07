cef82511916dbfda4dd9e3f5a6192048ac69f6b8
CanadianEngineer <alamper@uwo.ca>
2016-04-06 23:01:39 -0400

    trying to display contents of vector

--- Ngrams/P1.cpp
+++ Ngrams/P1.cpp
@@ -1,10 +1,16 @@
 #include "P1.h"
 using namespace std;
 
+typedef string T;
+
 void P1(string fileName_1, string fileName_2, int n, int printNGrams){
 
 	cout << fileName_1 << endl;
 
-	vector<string> tokens;
+	vector<T> tokens;
+	read_tokens(fileName_1, tokens, false); // reads tokens from file without EOS
 
+	for (int i = 0; i < tokens.size(); i++){
+		cout << tokens[i];
+	}
 }
