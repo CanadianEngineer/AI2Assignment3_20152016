b7a63596285a25b1ac9123cb178189c85be657e8
Andrew Lampert <alamper@uwo.ca>
2016-04-06 22:41:40 -0400

    initial commit

---
+++ Ngrams/P1.cpp
@@ -0,0 +1,10 @@
+#include "P1.h"
+using namespace std;
+
+void P1(string fileName_1, string fileName_2, int n, int printNGrams){
+
+	cout << fileName_1 << endl;
+
+	vector<string> tokens;
+
+}
---
+++ Ngrams/P1.h
@@ -0,0 +1,7 @@
+#include <string>
+#include <unordered_map>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+void P1(string fileName_1, string fileName_2, int n, int printNGrams);
